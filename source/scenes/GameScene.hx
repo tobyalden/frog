32ac73f866f9b4d50b22bf483823627591653e9e
Toby Alden <toby.alden@gmail.com>
2025-08-15 14:54:21 -0700

    Add infinite scroll

--- source/scenes/GameScene.hx
+++ source/scenes/GameScene.hx
@@ -14,22 +14,53 @@ import openfl.Assets;
 
 class GameScene extends Scene
 {
+    public static inline var GAME_WIDTH = 320;
+    public static inline var GAME_HEIGHT = 180;
+
     private var curtain:Curtain;
+    private var player:Player;
+    private var chunks:Array<Level>;
 
     override public function begin() {
         curtain = new Curtain();
         add(curtain);
         curtain.fadeOut(0.25);
 
-        var level = new Level("level");
-        add(level);
-        for(entity in level.entities) {
+        var start = new Level("start");
+        add(start);
+        for(entity in start.entities) {
+            if(Type.getClass(entity) == Player) {
+                player = cast(entity, Player);
+            }
             add(entity);
         }
+        chunks = [start];
     }
 
     override public function update() {
+        if(player.centerX + GAME_WIDTH > getTotalChunkWidth()) {
+            addChunk();
+        }
         super.update();
+        camera.x = player.centerX - GAME_WIDTH / 2;
+        if(Key.pressed(Key.R)) {
+            HXP.scene = new GameScene();
+        }
+    }
+
+    private function getTotalChunkWidth() {
+        var totalChunkWidth = 0;
+        for(chunk in chunks) {
+            totalChunkWidth += chunk.width;
+        }
+        return totalChunkWidth;
+    }
+
+    private function addChunk() {
+        var chunk = new Level("0");
+        chunk.x = getTotalChunkWidth();
+        chunks.push(chunk);
+        add(chunk);
     }
 
     public function onDeath() {
